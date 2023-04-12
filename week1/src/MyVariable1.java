fcfe08700c9973ecb39bc854a6740d7b9edc2806
way9z <97198962+way9z@users.noreply.github.com>
2023-04-12 11:04:40 -0700

    Add files via upload

---
+++ week1/src/MyVariable1.java
@@ -0,0 +1,11 @@
+public class MyVariable1 {
+
+public static void main(String[] args) {
+String name = "Wei"; // after a statement, don't forget the semi colon!
+    int hours = 40; //we are assinging 40 to the hours variable
+    //Please note, when we have a string vale, it is nested in ""
+    // when we are referring to numbers/integers, no quotes!!!
+    System.out.println(name);
+    System.out.println(hours);
+}
+}
