72de2413343c65953b68f51816a159bbde103a5b
way9z <97198962+way9z@users.noreply.github.com>
2023-04-13 11:01:28 -0700

    Add files via upload

---
+++ week1/src/MyVariable1.java
@@ -0,0 +1,39 @@
+public class MyVariable1 {
+
+public static void main(String[] args) {
+String name = "Wei"; // after a statement, don't forget the semi colon!
+    int hours = 40; //we are assigning 40 to the hours variable
+    //Please note, when we have a string vale, it is nested in ""
+    // when we are referring to numbers/integers, no quotes!!!
+    System.out.println(name);
+    System.out.println(hours);
+
+    String building, tree, lake, ocean;
+    tree = "Redwood";
+    building = "Empire State";
+    lake = "Lake Superior";
+    ocean = "Pacific Ocean";
+
+//    String building = "Empire State", tree = "Maple", Lake = "Lake Superior", Ocean = "Pacific Ocean";
+
+int weeks, decades, months, years;
+weeks = 4;
+decades = 10;
+months = 3;
+years = 5;
+
+//     in week = 4, decades =10, month = 3, years =5;
+
+    System.out.println(tree);
+    System.out.println(building);
+    System.out.println(lake);
+    System.out.println(ocean);
+    System.out.println(weeks);
+    System.out.println(decades);
+    System.out.println(months);
+    System.out.println(years);
+
+
+
+}
+}
